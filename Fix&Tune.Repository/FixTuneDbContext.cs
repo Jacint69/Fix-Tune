171dcf110ab85eee5fb7ac1c9df712f62c569b01
Jacint69 <kovacs.jacint02@gmail.com>
2024-10-13 18:00:34 +0200

    Admin user, using develop appsettings json read has been added.

--- Fix&Tune.Repository/FixTuneDbContext.cs
+++ Fix&Tune.Repository/FixTuneDbContext.cs
@@ -1,4 +1,5 @@
 ﻿using Fix_Tune.Models;
+using Microsoft.Extensions.Configuration;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
@@ -13,6 +14,7 @@ namespace Fix_Tune.Repository
         public DbSet<CarIssue> CarIssues {  get; set; }
         public virtual DbSet<User> Users {  get; set; }
 
+        private  IConfiguration _configuration;
 
         public FixTuneDbContext(DbContextOptions<FixTuneDbContext> opt) : base(opt) {}
 
@@ -25,6 +27,30 @@ namespace Fix_Tune.Repository
                 new { Id = "2", Name = "Mechanic", NormalizedName = "MECHANIC" },
                 new { Id = "3", Name = "Customer", NormalizedName = "CUSTOMER" }
                 );
+
+            //Create password hasher
+            PasswordHasher<User> passwordHasher = new PasswordHasher<User>();
+
+            //Load appsettings_develop json
+            var confbuilder = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings_develop.json", optional: false, reloadOnChange: true);
+            _configuration= confbuilder.Build();
+
+
+            //Admin user
+            User user = new User()
+            {
+                Id = Guid.NewGuid().ToString(),
+                Email = _configuration["UserSettings:Email"],
+                UserName = _configuration["UserSettings:UserName"],
+                FirstName="Kovács",
+                LastName="Jácint",
+                NormalizedUserName = _configuration["UserSettings:NormalizedUserName"]
+            };
+
+            user.PasswordHash = passwordHasher.HashPassword(user, _configuration["UserSettings:Password"]);
+            builder.Entity<User>().HasData(user);
 
             //Cars <--> Issue N:N CarIssue 
             builder.Entity<Issue>()
