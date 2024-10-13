a74b57f93f8f48a2caf2b25c53660fcc072a0752
Jacint69 <kovacs.jacint02@gmail.com>
2024-10-13 20:17:49 +0200

    JSONIGNORE attribute has been added.

--- Fix&Tune.Models/User.cs
+++ Fix&Tune.Models/User.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
+using System.Text.Json.Serialization;
 using System.Threading.Tasks;
 
 namespace Fix_Tune.Models
@@ -17,7 +18,7 @@ namespace Fix_Tune.Models
         public string LastName { get; set; }
 
         public DateTime DateOfRegistration {  get; set; }
-
+     
         public virtual ICollection<Car> Cars { get; set; }
 
         public User()
