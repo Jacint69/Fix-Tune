141f3d366050487c215f75e38f33f2512de97b25
Jacint69 <kovacs.jacint02@gmail.com>
2024-10-13 20:16:45 +0200

    JWT Token login created.

---
+++ Fix&Tune.Endpoint/Controllers/AuthController.cs
@@ -0,0 +1,49 @@
+﻿using Fix_Tune.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace Fix_Tune.Endpoint.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class AuthController:ControllerBase
+    {
+        private readonly UserManager<User> _userManager; //UserManager az identitybe van
+
+        public AuthController(UserManager<User> user)
+        {
+            _userManager = user;
+        }
+
+
+        [HttpPost]
+        public async Task<IActionResult> Login([FromBody] LoginViewModel model)
+        {
+            var user = await _userManager.FindByNameAsync(model.UserName);
+            if (user != null && await _userManager.CheckPasswordAsync(user, model.Password))
+            {
+                var claim = new List<Claim> { new Claim(JwtRegisteredClaimNames.Sub, user.UserName) };
+                foreach (var role in await _userManager.GetRolesAsync(user))
+                {
+                    claim.Add(new Claim(ClaimTypes.Role, role));
+                }
+                var signinKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("nagyonhosszutitkoskodhelyeasdasdasd"));
+                var token = new JwtSecurityToken(
+                issuer: "http://www.security.org", audience: "http://www.security.org",
+                claims: claim, expires: DateTime.Now.AddMinutes(60),
+                signingCredentials: new SigningCredentials(signinKey, SecurityAlgorithms.HmacSha256)
+                );
+                return Ok(new
+                {
+                    token = new JwtSecurityTokenHandler().WriteToken(token),
+                    expiration = token.ValidTo
+                });
+            }
+            return Unauthorized();
+        }
+    }
+}
