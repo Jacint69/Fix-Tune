362feb6cd443a90fafac67e7f5a7d4dd388b6730
Jacint69 <kovacs.jacint02@gmail.com>
2024-10-13 11:59:35 +0200

    User model has been created.

---
+++ Fix&Tune.Logic/User.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fix_Tune.Models
+{
+    public class User : IdentityUser
+    {
+        [StringLength(100)]
+        public string FirstName { get; set; }
+
+        [StringLength(100)]
+        public string LastName { get; set; }
+
+        public DateTime DateOfRegistration {  get; set; }
+
+        public virtual ICollection<Car> Cars { get; set; }
+
+        public User()
+        {
+            Cars=new HashSet<Car>();
+        }
+
+
+    }
+}
