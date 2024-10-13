134b9622fad353667cedcf7a72e6f8ae92aa75fa
Jacint69 <kovacs.jacint02@gmail.com>
2024-10-13 11:59:50 +0200

    CarIssue model has been created.

---
+++ Fix&Tune.Logic/CarIssue.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fix_Tune.Models
+{
+    [Table("CarIssues")]
+    internal class CarIssue
+    {
+        public int CarIssueId {  get; set; }
+
+        [ForeignKey(nameof(Car))]
+        public int CarId { get; set; }
+
+        [ForeignKey(nameof(Issue))]
+        public int IssueId { get; set; }
+
+        public virtual Car Car { get; set; }
+        public virtual Issue Issue { get; set; }    
+    }
+}
