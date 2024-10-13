5a32002306ff07eb54ccc29baf3dcb124d6f71e6
Jacint69 <kovacs.jacint02@gmail.com>
2024-10-13 13:31:42 +0200

    Repository abstract base class has been added.

---
+++ Fix&Tune.Repository/Repository.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fix_Tune.Repository
+{
+    public abstract class Repository<T> : IRepository<T> where T : class
+    {
+        protected FixTuneDbContext db;
+        protected Repository(FixTuneDbContext db)
+        {
+            this.db = db;   
+        }
+        public void Create(T entity)
+        {
+            db.Set<T>().Add(entity);
+            db.SaveChanges();
+        }
+
+        public void Delete(int id)
+        {
+            db.Set<T>().Remove(Read(id));
+        }
+
+        public abstract T Read(int id);
+        
+        
+        public IQueryable<T> ReadAll()
+        {
+            return db.Set<T>();
+        }
+
+        public abstract void Update(T entity);
+        
+    }
+}
