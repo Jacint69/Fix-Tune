1d8328297785e04a37018def99e6f11d8f9433b5
Jacint69 <kovacs.jacint02@gmail.com>
2024-10-13 21:36:31 +0200

    CarIssueLogic, interface has been added.

---
+++ Fix&Tune.Logic/CarIssueLogic.cs
@@ -0,0 +1,45 @@
+﻿using Fix_Tune.Models;
+using Fix_Tune.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fix_Tune.Logic
+{
+    public class CarIssueLogic : ICarIssueLogic
+    {
+        IRepository<CarIssue> repo;
+
+        public CarIssueLogic(IRepository<CarIssue> repo)
+        {
+            this.repo = repo;
+        }
+
+        public void Create(CarIssue entity)
+        {
+            repo.Create(entity);
+        }
+
+        public void Delete(int id)
+        {
+            repo.Delete(id);
+        }
+
+        public CarIssue Read(int id)
+        {
+            return repo.Read(id);
+        }
+
+        public IQueryable<CarIssue> ReadAll()
+        {
+            return repo.ReadAll();
+        }
+
+        public void Update(CarIssue entity)
+        {
+            repo.Update(entity);
+        }
+    }
+}
---
+++ Fix&Tune.Logic/ICarIssueLogic.cs
@@ -0,0 +1,13 @@
+﻿using Fix_Tune.Models;
+
+namespace Fix_Tune.Logic
+{
+    public interface ICarIssueLogic
+    {
+        void Create(CarIssue entity);
+        void Delete(int id);
+        CarIssue Read(int id);
+        IQueryable<CarIssue> ReadAll();
+        void Update(CarIssue entity);
+    }
+}
