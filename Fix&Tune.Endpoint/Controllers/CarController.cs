29323d501db7050a5ff640e151280896d447113e
Jacint69 <kovacs.jacint02@gmail.com>
2024-10-13 20:16:56 +0200

    Car controller class created.

---
+++ Fix&Tune.Endpoint/Controllers/CarController.cs
@@ -0,0 +1,26 @@
+﻿using Fix_Tune.Models;
+using Fix_Tune.Repository;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Fix_Tune.Endpoint.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class CarController : ControllerBase
+    {
+        IRepository<Car> carRepo;
+        public CarController(IRepository<Car> car)
+        {
+            carRepo = car;
+        }
+
+        [HttpGet]
+        [Authorize]
+        public IEnumerable<Car> GetCars()
+        {
+            return carRepo.ReadAll();
+        }
+    }
+}
