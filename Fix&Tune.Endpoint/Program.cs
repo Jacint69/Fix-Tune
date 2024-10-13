67c7f7e8e43ee7a10bff17c49ee7e863229e7701
Jacint69 <kovacs.jacint02@gmail.com>
2024-10-13 23:49:33 +0200

    Services, JWT token added.

--- Fix&Tune.Endpoint/Program.cs
+++ Fix&Tune.Endpoint/Program.cs
@@ -1,4 +1,13 @@
 
+using Fix_Tune.Logic;
+using Fix_Tune.Models;
+using Fix_Tune.Repository;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+
 namespace Fix_Tune.Endpoint
 {
     public class Program
@@ -14,9 +23,56 @@ namespace Fix_Tune.Endpoint
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
 
-            //sql, identity
+            builder.Services.AddTransient<IRepository<Car>, CarRepository>();
+            builder.Services.AddTransient<ICarLogic, CarLogic>();
 
-            builder.Services.
+            builder.Services.AddTransient<IRepository<Issue>, IssueRepository>();
+            builder.Services.AddTransient<IIssueLogic, IssueLogic>();
+
+            builder.Services.AddTransient<IRepository<CarIssue>, CarIssueRepository>();
+            builder.Services.AddTransient<ICarIssueLogic, CarIssueLogic>();
+
+
+            //sql, identity
+            string conn = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings_develop.json", optional: false, reloadOnChange: true)
+                .Build()
+                .GetConnectionString("DefaultConnection");
+            builder.Services.AddDbContext<FixTuneDbContext>(opt =>
+            {
+                opt.UseMySql(conn, new MySqlServerVersion("8.0.30")).UseLazyLoadingProxies();
+            });
+
+            builder.Services.AddIdentity<User, IdentityRole>(opt =>
+            {
+                opt.Password.RequireNonAlphanumeric = false;
+                opt.Password.RequiredLength = 8;
+            }).AddEntityFrameworkStores<FixTuneDbContext>()
+            .AddDefaultTokenProviders();
+
+            //JWT token
+
+            builder.Services.AddAuthentication(option =>
+            {
+                option.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                option.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+                option.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+            }).AddJwtBearer(options =>
+            {
+                options.SaveToken = true;
+                options.RequireHttpsMetadata = true;
+                options.TokenValidationParameters = new TokenValidationParameters()
+                {
+                    ValidateIssuer = true,
+                    ValidateAudience = true,
+                    ValidAudience = "http://www.security.org",
+                    ValidIssuer = "http://www.security.org",
+                    IssuerSigningKey = new SymmetricSecurityKey
+                (Encoding.UTF8.GetBytes("nagyonhosszutitkoskodhelyeasdasdasd"))
+                };
+            });
+
 
             var app = builder.Build();
 
@@ -27,7 +83,9 @@ namespace Fix_Tune.Endpoint
                 app.UseSwaggerUI();
             }
 
+            app.UseAuthentication();
             app.UseAuthorization();
+
 
 
             app.MapControllers();
