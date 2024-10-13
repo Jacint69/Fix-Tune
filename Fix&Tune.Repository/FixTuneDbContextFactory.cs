a3854feb8775b46b8232230f6e8ad11b2550b2f6
Jacint69 <kovacs.jacint02@gmail.com>
2024-10-13 18:47:10 +0200

    DbFactory has been created.

---
+++ Fix&Tune.Repository/FixTuneDbContextFactory.cs
@@ -0,0 +1,32 @@
+﻿using Castle.Core.Configuration;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+using System.IO;
+
+namespace Fix_Tune.Repository
+{
+    public class FixTuneDbContextFactory : IDesignTimeDbContextFactory<FixTuneDbContext>
+    {
+        public FixTuneDbContext CreateDbContext(string[] args)
+        {
+            // Konfigurációs beállítások betöltése
+            var configuration = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings_develop.json") // Beolvassa az appsettings.json fájlt
+                .Build();
+
+            var confbuilder = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings_develop.json", optional: false, reloadOnChange: true);
+            
+
+            // DbContextOptionsBuilder létrehozása
+            var optionsBuilder = new DbContextOptionsBuilder<FixTuneDbContext>();
+            var connectionString = configuration.GetConnectionString("DefaultConnection"); // Kiolvassa a connection stringet
+            optionsBuilder.UseMySql(connectionString, new MySqlServerVersion("8.0.30")); // Beállítja a MySQL adatbázis kapcsolatot
+
+            return new FixTuneDbContext(optionsBuilder.Options); // Visszaadja a FixTuneDbContext példányát
+        }
+    }
+}
