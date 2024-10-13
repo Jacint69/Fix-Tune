ba44c74db4edfd1a39fb40ce6af35ed69f545b48
Jacint69 <kovacs.jacint02@gmail.com>
2024-10-13 11:59:29 +0200

    Car model has been created.

--- Fix&Tune.Logic/Car.cs
+++ Fix&Tune.Logic/Car.cs
@@ -1,8 +1,34 @@
-﻿namespace Fix_Tune.Logic
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text.Json.Serialization;
+
+namespace Fix_Tune.Models
 {
+    [Table("Cars")]
     public class Car
     {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int CarId { get; set; }
+        [Required]
+        public string Brand { get; set; }
+        [Required]
+        public string Type { get; set; }
         
+        [Required]
+        [ForeignKey(nameof(User))]
+        public string UserId { get; set; }
+
+        public virtual User User { get; set; } 
+
+        [JsonIgnore]
+        public virtual ICollection<Issue> Issues { get; set; }
+
+        public Car()
+        {
+            Issues=new HashSet<Issue>();
+        }
+
 
     }
 }
