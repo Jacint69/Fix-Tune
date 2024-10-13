b3007f7f0025b9f8b2175860b9bdcf825bc02c16
Jacint69 <kovacs.jacint02@gmail.com>
2024-10-13 21:36:43 +0200

    Carlogic, interface has been added.

---
+++ Fix&Tune.Logic/CarLogic.cs
@@ -0,0 +1,39 @@
+﻿using Fix_Tune.Models;
+using Fix_Tune.Repository;
+
+namespace Fix_Tune.Logic
+{
+    public class CarLogic : ICarLogic
+    {
+        IRepository<Car> repo;
+        public CarLogic(IRepository<Car> repo)
+        {
+            this.repo = repo;
+        }
+
+        public void Create(Car entity)
+        {
+            repo.Create(entity);
+        }
+
+        public void Delete(int id)
+        {
+            repo.Delete(id);
+        }
+
+        public Car Read(int id)
+        {
+            return repo.Read(id);
+        }
+
+        public IQueryable<Car> ReadAll()
+        {
+            return repo.ReadAll();
+        }
+
+        public void Update(Car entity)
+        {
+            repo.Update(entity);
+        }
+    }
+}
---
+++ Fix&Tune.Logic/ICarLogic.cs
@@ -0,0 +1,13 @@
+﻿using Fix_Tune.Models;
+
+namespace Fix_Tune.Logic
+{
+    public interface ICarLogic
+    {
+        void Create(Car entity);
+        void Delete(int id);
+        Car Read(int id);
+        IQueryable<Car> ReadAll();
+        void Update(Car entity);
+    }
+}
