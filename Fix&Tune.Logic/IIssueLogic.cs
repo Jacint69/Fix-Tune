0b2253b1ec0631847af5614aed94026e2abe8e90
Jacint69 <kovacs.jacint02@gmail.com>
2024-10-13 21:36:59 +0200

    Issuelogic, interface has been added.

---
+++ Fix&Tune.Logic/IIssueLogic.cs
@@ -0,0 +1,12 @@
+﻿using Fix_Tune.Models;
+
+namespace Fix_Tune.Logic
+{
+    public interface IIssueLogic
+    {
+        void Create(Issue entity);
+        Issue Read(int id);
+        IQueryable<Issue> ReadAll();
+        void Update(Issue entity);
+    }
+}
---
+++ Fix&Tune.Logic/IssueLogic.cs
@@ -0,0 +1,34 @@
+﻿using Fix_Tune.Models;
+using Fix_Tune.Repository;
+
+namespace Fix_Tune.Logic
+{
+    public class IssueLogic : IIssueLogic
+    {
+        IRepository<Issue> repo;
+        public IssueLogic(IRepository<Issue> repo)
+        {
+            this.repo = repo;
+        }
+
+        public void Create(Issue entity)
+        {
+            repo.Create(entity);
+        }
+
+        public void Update(Issue entity)
+        {
+            repo.Update(entity);
+        }
+
+        public Issue Read(int id)
+        {
+            return repo.Read(id);
+        }
+
+        public IQueryable<Issue> ReadAll()
+        {
+            return repo.ReadAll();
+        }
+    }
+}
