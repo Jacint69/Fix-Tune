122f2230196a6cc8b3d5fdd2886310aa2e7d31dd
Jacint69 <kovacs.jacint02@gmail.com>
2024-10-13 11:59:43 +0200

    Issue model has been created.

---
+++ Fix&Tune.Logic/Issue.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace Fix_Tune.Models
+{
+    [Table("Issues")]
+    public class Issue
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int IssueID { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public string Description { get; set; }
+        [JsonIgnore]
+        public virtual ICollection<Car> Cars { get; set; }
+
+        public Issue()
+        {
+            Cars=new HashSet<Car>();
+        }
+    }
+}
