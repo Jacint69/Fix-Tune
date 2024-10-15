182d2da8f5baf1a5df2ddffbb5f3598bb27b380f
Jacint69 <kovacs.jacint02@gmail.com>
2024-10-15 15:23:46 +0200

    CarDTO, IssueDTO classes created.

---
+++ Fix&Tune.Models/CarDTO.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fix_Tune.Models
+{
+    public class CarDTO
+    {
+        public int CarId { get; set; }
+        public string Brand { get; set; }
+        public string Type { get; set; }
+        public int Year { get; set; }
+        public string PlateNumber { get; set; }
+        public string UserId { get; set; }
+        public List<IssueDTO> Issues { get; set; }
+    }
+}
---
+++ Fix&Tune.Models/IssueDTO.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace Fix_Tune.Models
+{
+    public class IssueDTO
+    {
+        
+        public int IssueID { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        
+
+    }
+}
