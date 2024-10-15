1f9b6e5962c130998bec34fecc94ee85c19db19c
Jacint69 <kovacs.jacint02@gmail.com>
2024-10-15 17:57:52 +0200

    Created Rest methods.

--- Fix&Tune.Endpoint/Controllers/CarIssueController.cs
+++ Fix&Tune.Endpoint/Controllers/CarIssueController.cs
@@ -1,6 +1,44 @@
-﻿namespace Fix_Tune.Endpoint.Controllers
+﻿using Fix_Tune.Logic;
+using Fix_Tune.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Fix_Tune.Endpoint.Controllers
 {
-    public class CarIssueController
+    [Authorize]
+    [ApiController]
+    [Route("[controller]")]
+    public class CarIssueController:ControllerBase
     {
+        ICarIssueLogic carIssueLogic;
+        public CarIssueController(ICarIssueLogic carIssueLogic)
+        {
+            this.carIssueLogic = carIssueLogic;
+        }
+
+        [HttpGet]
+        public IEnumerable<CarIssue> CarIssues()
+        {
+            return carIssueLogic.ReadAll();
+        }
+
+        [HttpGet("{id}")]
+        public CarIssue CarIssue(int id) { 
+            return carIssueLogic.Read(id);
+        }
+
+        [HttpPut]
+        public IActionResult UpdateCarIssue(CarIssue carIssue)
+        {
+            carIssueLogic.Update(carIssue);
+            return Ok(carIssue);
+        }
+
+        [HttpDelete]
+        public IActionResult DeleteCarIssue(int id)
+        {
+            carIssueLogic.Delete(id);
+            return Ok("Deleted: " + id);
+        }
     }
 }
