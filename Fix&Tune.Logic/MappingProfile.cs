d9a8fddc6ee5cb51370e494053731a8e67f9b0fc
Jacint69 <kovacs.jacint02@gmail.com>
2024-10-15 15:23:13 +0200

    Mapping Profile (DTO) has been added.

---
+++ Fix&Tune.Logic/MappingProfile.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using Fix_Tune.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fix_Tune.Logic
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile() {
+
+            CreateMap<Car, CarDTO>();
+            CreateMap<Issue, IssueDTO>();
+        }
+    }
+}
