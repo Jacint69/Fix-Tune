3314885949e07fd7fd42aecf3b4a9bd5dee4e7ec
Jacint69 <kovacs.jacint02@gmail.com>
2024-10-16 11:35:52 +0200

    Created mechanic logic.

---
+++ Fix&Tune.Logic/MechanicLogic.cs
@@ -0,0 +1,34 @@
+﻿using Fix_Tune.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fix_Tune.Logic
+{
+    public class MechanicLogic
+    {
+        ICarLogic carLogic;
+        IIssueLogic issueLogic;
+
+        public MechanicLogic(ICarLogic carLogic, IIssueLogic issueLogic)
+        {
+            this.carLogic = carLogic;
+            this.issueLogic = issueLogic;
+        }
+
+        public IEnumerable<Car> FixNeedCars()
+        {
+            return carLogic.ReadAll().Where(t => !t.Status);
+        }
+
+        public IEnumerable<Car> FixedCars() {
+            return carLogic.ReadAll().Where(t => t.Status);
+        }
+
+
+
+
+    }
+}
