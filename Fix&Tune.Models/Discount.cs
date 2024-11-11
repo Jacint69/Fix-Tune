613c1bb32429e2ef74f6a9059f0c23973aed0cd5
Jacint69 <kovacs.jacint02@gmail.com>
2024-11-11 23:25:18 +0100

    Discount modell has been created.

---
+++ Fix&Tune.Models/Discount.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fix_Tune.Models
+{
+    internal class Discount
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int DiscountId { get; set; }
+        [ForeignKey(nameof(Service))]
+        public int ServiceId;
+
+        public virtual Service Service { get; set; }
+
+
+    }
+}
