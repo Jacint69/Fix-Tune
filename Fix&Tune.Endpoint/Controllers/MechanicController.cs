f75c97d3dd70edbd37308ef68732ea22f7df4dad
Jacint69 <kovacs.jacint02@gmail.com>
2024-11-13 16:38:10 +0100

    MechanicController has been created.

---
+++ Fix&Tune.Endpoint/Controllers/MechanicController.cs
@@ -0,0 +1,30 @@
+﻿using Fix_Tune.Logic;
+using Fix_Tune.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Fix_Tune.Endpoint.Controllers
+{
+    [Authorize(Roles = "Admin, Mechanic")]
+    [Route("[controller]/[action]")]
+    public class MechanicController : ControllerBase
+    {
+        IMechanicLogic mechanicLogic;
+        UserManager<User> _userManager;
+        public MechanicController(IMechanicLogic mechanicLogic, UserManager<User> userManager)
+        {
+            this.mechanicLogic = mechanicLogic;
+            this._userManager = userManager;
+        }
+
+        [HttpGet]//TODO: User átírása userid-ra
+        public async Task<IActionResult> GroupByCarsToUser()
+        {
+            var user = await _userManager.FindByNameAsync(this.User.Identity.Name);
+            var result = mechanicLogic.GroupByCarsToUser(user);
+            return Ok(result);
+
+        }
+    }
+}
