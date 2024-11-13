f42955a0f03961aab8f65aa2c18f0121e8260445
Jacint69 <kovacs.jacint02@gmail.com>
2024-11-13 13:34:49 +0100

    MechanicLogic added GroupByCarsToUser method.

---
+++ Fix&Tune.Logic/IMechanicLogic.cs
@@ -0,0 +1,11 @@
+﻿using Fix_Tune.Models;
+
+namespace Fix_Tune.Logic
+{
+    public interface IMechanicLogic
+    {
+        IEnumerable<Car> FixedCars();
+        IEnumerable<Car> FixNeedCars();
+        IEnumerable<Car> GroupByCarsToUser(User user);
+    }
+}
--- Fix&Tune.Logic/MechanicLogic.cs
+++ Fix&Tune.Logic/MechanicLogic.cs
@@ -1,4 +1,5 @@
 ﻿using Fix_Tune.Models;
+using Microsoft.AspNetCore.Identity;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -7,15 +8,17 @@ using System.Threading.Tasks;
 
 namespace Fix_Tune.Logic
 {
-    public class MechanicLogic
+    public class MechanicLogic : IMechanicLogic
     {
         ICarLogic carLogic;
         IIssueLogic issueLogic;
+        UserManager<User> _userManager;
 
-        public MechanicLogic(ICarLogic carLogic, IIssueLogic issueLogic)
+        public MechanicLogic(ICarLogic carLogic, IIssueLogic issueLogic, UserManager<User> userManager)
         {
             this.carLogic = carLogic;
             this.issueLogic = issueLogic;
+            _userManager = userManager;
         }
 
         public IEnumerable<Car> FixNeedCars()
@@ -23,8 +26,24 @@ namespace Fix_Tune.Logic
             return carLogic.ReadAll().Where(t => !t.Status);
         }
 
-        public IEnumerable<Car> FixedCars() {
+        public IEnumerable<Car> FixedCars()
+        {
             return carLogic.ReadAll().Where(t => t.Status);
+        }
+
+        public IEnumerable<Car> GroupByCarsToUser(User user)
+        {
+            var items = carLogic.ReadAll().Where(t => t.UserId == user.Id).GroupBy(x => x.UserId).ToList();
+            List<Car> cars = new List<Car>();
+            foreach (var item in items)
+            {
+                foreach (var car in item)
+                {
+                    cars.Add(car);
+                }
+
+            }
+            return cars;
         }
 
 
