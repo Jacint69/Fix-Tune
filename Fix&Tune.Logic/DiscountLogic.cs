3a2d8c7695a791faa282179c52acfa9fc20fa97f
Jacint69 <kovacs.jacint02@gmail.com>
2024-11-13 13:24:49 +0100

    DiscountLogic, interface has been created.

---
+++ Fix&Tune.Logic/DiscountLogic.cs
@@ -0,0 +1,43 @@
+﻿using Fix_Tune.Models;
+using Fix_Tune.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fix_Tune.Logic
+{
+    internal class DiscountLogic : IDiscountLogic
+    {
+        IRepository<Discount> repo;
+        public DiscountLogic(IRepository<Discount> repo)
+        {
+            this.repo = repo;
+        }
+        public void Create(Discount entity)
+        {
+            repo.Create(entity);
+        }
+
+        public void Delete(int id)
+        {
+            repo.Delete(id);
+        }
+
+        public Discount Read(int id)
+        {
+            return repo.Read(id);
+        }
+
+        public IQueryable<Discount> ReadAll()
+        {
+            return repo.ReadAll();
+        }
+
+        public void Update(Discount entity)
+        {
+            repo.Update(entity);
+        }
+    }
+}
---
+++ Fix&Tune.Logic/IDiscountLogic.cs
@@ -0,0 +1,13 @@
+﻿using Fix_Tune.Models;
+
+namespace Fix_Tune.Logic
+{
+    internal interface IDiscountLogic
+    {
+        void Create(Discount entity);
+        void Delete(int id);
+        Discount Read(int id);
+        IQueryable<Discount> ReadAll();
+        void Update(Discount entity);
+    }
+}
