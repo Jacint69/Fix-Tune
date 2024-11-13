6ebcd8b9c3102527deac0813c9a769cc358dba49
Jacint69 <kovacs.jacint02@gmail.com>
2024-11-13 15:23:30 +0100

    Discount controller has been created.

---
+++ Fix&Tune.Endpoint/Controllers/DiscountController.cs
@@ -0,0 +1,45 @@
+﻿using Fix_Tune.Logic;
+using Fix_Tune.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Fix_Tune.Endpoint.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class DiscountController :ControllerBase
+    {
+        IDiscountLogic logic;
+        public DiscountController(IDiscountLogic logic)
+        {
+            this.logic= logic;
+        }
+
+        [HttpGet]
+        public IEnumerable<Discount> GetDiscounts()
+        {
+            return logic.ReadAll();
+        }
+
+        [HttpGet("{id}")]
+        public Discount GetDiscount(int id)
+        {
+            return logic.Read(id);
+        }
+
+        [Authorize(Roles ="Admin")]
+        [HttpDelete("{id}")]
+        public void Delete(int id) { 
+            logic.Delete(id);
+        }
+
+        [Authorize(Roles = "Admin")]
+        [HttpPost]
+        public void Create([FromBody] Discount service)
+        {
+            logic.Create(service);
+        }
+
+    }
+}
