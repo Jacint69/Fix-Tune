2a19082d91399b49c85ea527910790376687f3d0
Jacint69 <kovacs.jacint02@gmail.com>
2024-11-13 16:36:53 +0100

    Workshop logic, interface, created.

---
+++ Fix&Tune.Logic/IWorkshopLogic.cs
@@ -0,0 +1,13 @@
+﻿using Fix_Tune.Models;
+
+namespace Fix_Tune.Logic
+{
+    public interface IWorkshopLogic
+    {
+        void Create(Workshop entity);
+        void Delete(int id);
+        Workshop Read(int id);
+        IQueryable<Workshop> ReadAll();
+        void Update(Workshop entity);
+    }
+}
---
+++ Fix&Tune.Logic/WorkshopLogic.cs
@@ -0,0 +1,44 @@
+﻿using Fix_Tune.Models;
+using Fix_Tune.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fix_Tune.Logic
+{
+    public class WorkshopLogic : IWorkshopLogic
+    {
+
+        IRepository<Workshop> repo;
+        public WorkshopLogic(IRepository<Workshop> repo)
+        {
+            this.repo = repo;
+        }
+        public void Create(Workshop entity)
+        {
+            repo.Create(entity);
+        }
+
+        public void Delete(int id)
+        {
+            repo.Delete(id);
+        }
+
+        public Workshop Read(int id)
+        {
+            return repo.Read(id);
+        }
+
+        public IQueryable<Workshop> ReadAll()
+        {
+            return repo.ReadAll();
+        }
+
+        public void Update(Workshop entity)
+        {
+            repo.Update(entity);
+        }
+    }
+}
