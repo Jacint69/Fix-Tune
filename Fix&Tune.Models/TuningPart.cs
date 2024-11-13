7831ddd3701a9c74541d73e21d800adc7609a621
Jacint69 <kovacs.jacint02@gmail.com>
2024-11-13 15:25:31 +0100

    TuningPart model has been created.

---
+++ Fix&Tune.Models/TuningPart.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fix_Tune.Models
+{
+    public enum TuningPartType
+    {
+        DieselInjector, Turbo, InterCooler, Camshaft
+    }
+    public class TuningPart
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int TuningPartID {  get; set; }
+
+        [Required]
+        public TuningPartType TuningPartType { get; set;}
+
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        [Length(1000,1000000)]
+        public int Price { get; set; }
+
+
+    }
+}
