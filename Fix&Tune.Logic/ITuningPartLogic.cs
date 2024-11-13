a8dc85ca1b48333e7fe977af819644b84c7fcbe9
Jacint69 <kovacs.jacint02@gmail.com>
2024-11-13 16:36:38 +0100

    TuningPart logic, interface, created.

---
+++ Fix&Tune.Logic/ITuningPartLogic.cs
@@ -0,0 +1,13 @@
+﻿using Fix_Tune.Models;
+
+namespace Fix_Tune.Logic
+{
+    public interface ITuningPartLogic
+    {
+        void Create(TuningPart entity);
+        void Delete(int id);
+        TuningPart Read(int id);
+        IQueryable<TuningPart> ReadAll();
+        void Update(TuningPart entity);
+    }
+}
---
+++ Fix&Tune.Logic/TuningPartLogic.cs
@@ -0,0 +1,45 @@
+﻿using Fix_Tune.Models;
+using Fix_Tune.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fix_Tune.Logic
+{
+    public class TuningPartLogic : ITuningPartLogic
+    {
+        IRepository<TuningPart> repo;
+
+        public TuningPartLogic(IRepository<TuningPart> repo)
+        {
+            this.repo = repo;
+        }
+
+        public void Create(TuningPart entity)
+        {
+            repo.Create(entity);
+        }
+
+        public void Delete(int id)
+        {
+            repo.Delete(id);
+        }
+
+        public TuningPart Read(int id)
+        {
+            return repo.Read(id);
+        }
+
+        public IQueryable<TuningPart> ReadAll()
+        {
+            return repo.ReadAll();
+        }
+
+        public void Update(TuningPart entity)
+        {
+            repo.Update(entity);
+        }
+    }
+}
